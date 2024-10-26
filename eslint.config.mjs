86ab9c61af287438f8cb1d720a73c7b8b085d0ea
Bruno Peralva <brunoperalva@hotmail.com>
2024-10-26 15:32:07 -0300

    fix: correção no eslint; diversos modulos atualizados
    fix #7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
 	{ languageOptions: { globals: globals.node } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
